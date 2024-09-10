feab3135e438dc817ccd71984517d35fc5cfd6b8
y0001ha <yyy96110@naver.com>
2024-09-10 12:49:26 +0900

    20240910 12:50

--- src/main/java/com/korit/senicare/provider/jwtProvider.java
+++ src/main/java/com/korit/senicare/provider/jwtProvider.java
@@ -10,6 +10,8 @@ import java.nio.charset.StandardCharsets;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 
 // class: JWT 생성 및 검증 기능 제공자
@@ -18,7 +20,7 @@ import io.jsonwebtoken.security.Keys;
 // - JWT 만료기간 10시간
 
 @Component
-public class jwtProvider {
+public class JwtProvider {
 
     @Value("${jwt.secret}")
     private String secretKey;
@@ -36,12 +38,47 @@ public class jwtProvider {
             // JWT 암호화에 사용할 Key 생성
             Key key = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
 
+            // JWT 생성
+            jwt = Jwts.builder()
+                .signWith(key, SignatureAlgorithm.HS256)
+                .setSubject(userId)
+                .setIssuedAt(new Date())
+                . setExpiration(expiredDate)
+                .compact();
+
         } catch (Exception exception) {
             exception.printStackTrace();
             return null;
         }
 
         return jwt;
+
+    }
+
+    // JWT 검증 메서드
+    public String validate(String jwt) {
+
+        String userId = null;
+
+        try {
+
+             // JWT 암호화에 사용할 Key 생성
+            Key key = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
+
+            // JWT 검증 및 payload의 subject 값 추출
+            userId = Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(jwt)
+                .getBody()
+                .getSubject();
+
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            return null;
+        }
+
+        return userId;
 
     }
 }
