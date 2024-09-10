4960db8bc006af29d284ab3d0963d0354bf2f0ee
y0001ha <yyy96110@naver.com>
2024-09-10 14:54:07 +0900

    20240910 14:50

--- src/main/java/com/korit/senicare/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/korit/senicare/filter/JwtAuthenticationFilter.java
@@ -1,19 +1,35 @@
 package com.korit.senicare.filter;
 
 import java.io.IOException;
+import java.util.Collection;
 
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.korit.senicare.provider.JwtProvider;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 
 // JWT 검증 및 Security Context에 접근 제어자 추가 필터
 // - reqeust의 header에서 토큰 추출 검증
 // - security context에 접근 제어자 정보 등록
+@Component
+@RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtProvider jwtProvider;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
@@ -29,7 +45,15 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             }
 
             // 토큰 검증
-            
+            String userId = jwtProvider.validate(token);
+            if (userId == null) {
+                filterChain.doFilter(request, response);
+                return;
+            }
+
+            // security context에 등록
+            setContext(request, userId);
+
 
         } catch(Exception exception) {
             exception.printStackTrace();
@@ -56,6 +80,26 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         // Authorization 필드 값에서 토큰 추출
         String token = authorization.substring(7);
         return token; 
+    }
 
+    // security context 생성 및 등록
+    private void setContext(HttpServletRequest request, String userId) {
+
+        // 접근 주체에 대한 인증 토큰 생성
+        AbstractAuthenticationToken authenticationToken = 
+            new UsernamePasswordAuthenticationToken(userId, null, AuthorityUtils.NO_AUTHORITIES);
+
+        // 생성한 인증 토큰이 어떤 요청에 대한 내용인 상세 정보 추가
+        authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+        // 빈 security context 생성
+        SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
+
+        // 생성한 빈 security context에 autheticationToken 주입
+        securityContext.setAuthentication(authenticationToken);
+
+        // 생성한 security context 등록
+        SecurityContextHolder.setContext(securityContext);
     }
 }
+
