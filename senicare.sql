e97d5d0f3b70e76b81d8f289f6b24fc81e417a33
y0001ha <yyy96110@naver.com>
2024-09-10 11:35:22 +0900

    20240910 11:40

--- senicare.sql
+++ senicare.sql
@@ -126,3 +126,7 @@ COMMENT = '관리 기록 테이블';
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
+CREATE USER 'senicare'@'%' IDENTIFIED BY 'P!ssw0rd';
+
+GRANT all privileges ON  senicare.* TO 'senicare'@'%';
---
+++ src/main/java/com/korit/senicare/provider/jwtProvider.java
@@ -0,0 +1,11 @@
+package com.korit.senicare.provider;
+
+// class: JWT 생성 및 검증 기능 제공자
+// - JWT 암호화 알고리즘 HS256
+// - 비밀키 환경변수에 있는 jwt.secret
+// - JWT 만료기간 10시간
+
+
+public class jwtProvider {
+    
+}
