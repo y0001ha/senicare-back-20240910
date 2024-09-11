78925a667817ecd2adb83ddd71685d1a7039bfe5
y0001ha <yyy96110@naver.com>
2024-09-11 15:58:14 +0900

    20240911 15:50

--- src/main/java/com/korit/senicare/service/implement/AuthServiceImplement.java
+++ src/main/java/com/korit/senicare/service/implement/AuthServiceImplement.java
@@ -9,16 +9,18 @@ import com.korit.senicare.dto.request.auth.IdCheckRequestDto;
 import com.korit.senicare.dto.request.auth.TelAuthRequestDto;
 import com.korit.senicare.dto.response.ResponseDto;
 import com.korit.senicare.entity.TelAuthNumberEntity;
+import com.korit.senicare.provider.SmsProvider;
 import com.korit.senicare.repository.NurseRepository;
 import com.korit.senicare.repository.TelAuthNumberRepository;
 import com.korit.senicare.service.AuthService;
 
-import jakarta.el.ELException;
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class AuthServiceImplement implements AuthService {
+
+    private final SmsProvider smsProvider;
 
     private final NurseRepository nurseRepository;
     private final TelAuthNumberRepository telAuthNumberRepository;
@@ -58,6 +60,8 @@ public class AuthServiceImplement implements AuthService {
         }
 
         String authNumber = AuthNumberCreator.number4();
+
+        smsProvider.sendMessage(telNumber, authNumber);
 
         try {
 
