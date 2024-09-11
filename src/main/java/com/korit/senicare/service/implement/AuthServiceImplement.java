2fc944083f4d1adfe9206eb54280bc427624dc11
y0001ha <yyy96110@naver.com>
2024-09-11 10:52:01 +0900

    20240911 10:50

--- src/main/java/com/korit/senicare/service/implement/AuthServiceImplement.java
+++ src/main/java/com/korit/senicare/service/implement/AuthServiceImplement.java
@@ -1,5 +1,6 @@
 package com.korit.senicare.service.implement;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
@@ -25,11 +26,15 @@ public class AuthServiceImplement implements AuthService {
         try {
 
             boolean isExistedId = nurseRepository.existsByUserId(userId);
+            if (isExistedId) return ResponseDto.duplicatedUserID();
 
-        } catch(ELException exception) {
+        } catch(Exception exception) {
             exception.printStackTrace();
+            return ResponseDto.databaseError();
         }
 
-    }
+        return ResponseDto.success();
     
+    }
+
 }
