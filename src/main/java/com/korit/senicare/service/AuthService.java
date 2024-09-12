8a1551452a755402d03b06b037d147f94a7505b4
y0001ha <yyy96110@naver.com>
2024-09-12 09:50:58 +0900

    20240912 09:50

--- src/main/java/com/korit/senicare/service/AuthService.java
+++ src/main/java/com/korit/senicare/service/AuthService.java
@@ -3,6 +3,7 @@ package com.korit.senicare.service;
 import org.springframework.http.ResponseEntity;
 
 import com.korit.senicare.dto.request.auth.IdCheckRequestDto;
+import com.korit.senicare.dto.request.auth.SignUpRequestDto;
 import com.korit.senicare.dto.request.auth.TelAuthCheckRequestDto;
 import com.korit.senicare.dto.request.auth.TelAuthRequestDto;
 import com.korit.senicare.dto.response.ResponseDto;
@@ -12,4 +13,5 @@ public interface AuthService {
     ResponseEntity<ResponseDto> idCheck(IdCheckRequestDto dto);
     ResponseEntity<ResponseDto> telAuth(TelAuthRequestDto dto);
     ResponseEntity<ResponseDto> telAuthCheck(TelAuthCheckRequestDto dto);
+    ResponseEntity<ResponseDto> signUp(SignUpRequestDto dto);
 } 
