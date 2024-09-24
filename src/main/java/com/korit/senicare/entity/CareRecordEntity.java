1449fa1041f66f562cd6a59f0a5120062ce51e36
y0001ha <yyy96110@naver.com>
2024-09-24 14:52:56 +0900

    20240924 14:50

--- src/main/java/com/korit/senicare/entity/CareRecordEntity.java
+++ src/main/java/com/korit/senicare/entity/CareRecordEntity.java
@@ -23,7 +23,7 @@ public class CareRecordEntity {
     private Integer recordNumber;
     private String recordDate;
     private String contents;
-    private Integer usedTool;
+    private String usedToolName;
     private Integer count;
     private String charger;
     private Integer customNumber;
