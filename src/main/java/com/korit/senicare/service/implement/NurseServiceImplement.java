3984487ffbce5f1ce9fd2cb8d76ab8df49cb82d5
y0001ha <yyy96110@naver.com>
2024-09-30 17:17:03 +0900

    20240930 17:50

--- src/main/java/com/korit/senicare/service/implement/NurseServiceImplement.java
+++ src/main/java/com/korit/senicare/service/implement/NurseServiceImplement.java
@@ -8,10 +8,13 @@ import org.springframework.stereotype.Service;
 
 import com.korit.senicare.dto.request.nurse.PatchNurseRequestDto;
 import com.korit.senicare.dto.response.ResponseDto;
+import com.korit.senicare.dto.response.nurse.GetChargedCustomerResponseDto;
 import com.korit.senicare.dto.response.nurse.GetNurseListResponseDto;
 import com.korit.senicare.dto.response.nurse.GetNurseResponseDto;
 import com.korit.senicare.dto.response.nurse.GetSignInResponseDto;
+import com.korit.senicare.entity.CustomerEntity;
 import com.korit.senicare.entity.NurseEntity;
+import com.korit.senicare.repository.CustomerRepository;
 import com.korit.senicare.repository.NurseRepository;
 import com.korit.senicare.service.NurseService;
 
@@ -22,6 +25,7 @@ import lombok.RequiredArgsConstructor;
 public class NurseServiceImplement implements NurseService {
 
     private final NurseRepository nurseRepository;
+    private final CustomerRepository customerRepository;
 
     @Override
     public ResponseEntity<? super GetSignInResponseDto> getSignIn(String userId) {
@@ -98,6 +102,23 @@ public class NurseServiceImplement implements NurseService {
 
         return ResponseDto.success();
 
+    }
+
+    @Override
+    public ResponseEntity<? super GetChargedCustomerResponseDto> getChargedCustomer(String nurseId) {
+
+        List<CustomerEntity> customereEntities = new ArrayList<>();
+
+        try {
+            
+            customereEntities = customerRepository.findByCharger(nurseId);
+
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            return ResponseDto.databaseError();
+        }
+
+        return GetChargedCustomerResponseDto.success(customereEntities);
     }
     
 }
